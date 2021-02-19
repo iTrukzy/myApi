74495e5f9853eca7270833ba1d87801a0683ee67
william herrera <wjha.1998@hotmail.com>
2021-02-18 20:26:10 -0500

    first commit

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://https://william-api-create.netlify.app/pythonreactdb'
+app.config['MONGO_URI']='mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app)
 
 CORS(app)
